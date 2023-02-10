9eacfb165d84c3dbdd481de6e5a5dd96cacc83b5
Christian Rodriguez <christian796rodriguez@gmail.com>
2023-02-10 12:14:54 -0500

    Initial Commit

---
+++ GirlsTake/GirlsTakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GirlsTakeApp.swift
+//  GirlsTake
+//
+//  Created by Christian Rodriguez on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GirlsTakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
